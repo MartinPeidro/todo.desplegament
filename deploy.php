61c5eaeef33c29443c09bf5fb6282c265e176862
MartinPeidro <peidromartin@gmail.com>
2025-12-11 19:53:41 +0100

    [ADD] update composer.json

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@ require 'recipe/laravel.php';
 
 // Config
 
-set('repository', 'https://github.com/acoloma-edu/todo.git');
+set('repository', 'https://github.com/MartinPeidro/todo.desplegament.git');
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -13,19 +13,10 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('192.168.1.137')
+host('100.29.248.20')
     ->set('remote_user', 'ddaw-ud4-deployer')
-    ->set('deploy_path', '/var/www/ddaw-ud4-a4/html');
+    ->set('deploy_path', '~/var/www/ddaw-ud4-deployer/html');
 
 // Hooks
+
 after('deploy:failed', 'deploy:unlock');
-
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
-
-task('reiniciar-fpm', function () {
-    run('sudo /etc/init.d/php8.1-fpm restart');
-});
-
-after('deploy', 'reiniciar-fpm');
