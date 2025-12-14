7869062ca190c5ac78654fd915cd59ef27c288ee
MartinPeidro <peidromartin@gmail.com>
2025-12-14 11:25:59 +0100

    [*ADD] Feature a43534sg – CRUD usuaris

--- deploy.php
+++ deploy.php
@@ -14,9 +14,18 @@ add('writable_dirs', []);
 // Hosts
 
 host('100.29.248.20')
-    ->set('remote_user', 'ddaw-ud4-deployer')
-    ->set('deploy_path', '~/var/www/ddaw-ud4-deployer/html');
+    ->set('remote_user', 'sa04-deployer')
+    ->set('deploy_path', '/var/www/es-cipfpbatoi-deployer/html');
 
 // Hooks
 
+task('artisan:migrate', function () {
+ writeln('Skipping artisan:migrate');
+ });
+
+task('reload:php-fpm', function () {
+ run('sudo /etc/init.d/php8.3-fpm restart');
+});
+
+after('deploy', 'reload:php-fpm');
 after('deploy:failed', 'deploy:unlock');
